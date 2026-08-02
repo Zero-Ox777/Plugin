b1c2dbfa7e7146deabb754f306a50c3b2e3cb333
Zero-Ox777 <gamingfire3790@gmail.com>
2026-08-02 10:53:37 +0530

    Add files via upload

---
+++ index-v2.js
@@ -0,0 +1,8 @@
+module.exports = {
+  onLoad: function () {
+    console.log("[GoToPing] Plugin loaded successfully");
+  },
+  onUnload: function () {
+    console.log("[GoToPing] Plugin unloaded");
+  },
+};
